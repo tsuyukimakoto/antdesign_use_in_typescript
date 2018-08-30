7c2435b2e6c0a01106bdebe751f1818f202cbbad
makoto tsuyuki <mtsuyuki@gmail.com>
2018-08-31 02:07:31 +0900

    use HOC instead of class

--- antd-demo-ts/src/App.tsx
+++ antd-demo-ts/src/App.tsx
@@ -2,14 +2,10 @@ import { Button } from 'antd';
 import * as React from 'react';
 import './App.css';
 
-class App extends React.Component {
-  public render() {
-    return (
-      <div className="App">
-        <Button type="primary">Button</Button>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <Button type="primary">Button</Button>
+  </div>
+);
 
 export default App;
